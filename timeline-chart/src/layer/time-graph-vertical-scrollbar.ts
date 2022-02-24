be37b189d45f9b47f19c88305ec2b99cdbf77fc4
William Yang <william.yang@qrigroup.com>
2022-02-24 00:30:55 -0600

    Snap to mouse on time-graph-navigator click
    Snap to mouse on time-graph-navigator click
    
    Adds background PIXI rectangles to the time-graph-navigator +
    vertical-navigator components.  This rectangle listens for clicks then centers the handler on the mouse and resumes 'dragging' for navigating.
    
    Fixes: #97
    
    Signed-off-by: Will Yang <william.yang@ericsson.com>

--- timeline-chart/src/layer/time-graph-vertical-scrollbar.ts
+++ timeline-chart/src/layer/time-graph-vertical-scrollbar.ts
@@ -7,6 +7,7 @@ import { TimeGraphRowController } from "../time-graph-row-controller";
 export class TimeGraphVerticalScrollbar extends TimeGraphChartLayer {
 
     protected navigatorHandle: TimeGraphVerticalScrollbarHandle;
+    protected navigatorBackground: TimeGraphVerticalScrollbarBackground;
     protected selectionRange?: TimeGraphRectangle;
 
     protected factor: number;
@@ -18,11 +19,14 @@ export class TimeGraphVerticalScrollbar extends TimeGraphChartLayer {
     protected afterAddToContainer() {
         this.updateFactor();
         this.navigatorHandle = new TimeGraphVerticalScrollbarHandle(this.rowController, this.stateController, this.factor);
+        this.navigatorBackground = new TimeGraphVerticalScrollbarBackground(this.rowController, this.stateController, this.factor);
+        this.addChild(this.navigatorBackground);
         this.addChild(this.navigatorHandle);
         this.rowController.onVerticalOffsetChangedHandler(() => this.update());
         this.rowController.onTotalHeightChangedHandler(() => {
             this.updateFactor();
             this.navigatorHandle.updateFactor(this.factor);
+            this.navigatorBackground.updateFactor(this.factor);
             this.update()
         });
     }
@@ -38,6 +42,8 @@ export class TimeGraphVerticalScrollbar extends TimeGraphChartLayer {
     update() {
         this.navigatorHandle.clear();
         this.navigatorHandle.render();
+        this.navigatorBackground.clear();
+        this.navigatorBackground.render();
     }
 }
 
@@ -52,11 +58,21 @@ export class TimeGraphVerticalScrollbarHandle extends TimeGraphComponent<null> {
 
     constructor(protected rowController: TimeGraphRowController, protected stateController: TimeGraphStateController, protected factor: number) {
         super('vscroll_handle');
-        this.addEvent('mousedown', event => {
+        const moveStart: TimeGraphInteractionHandler = event => {
             this.mouseStartY = event.data.global.y;
-            this.oldVerticalOffset = this.rowController.verticalOffset
+            this.oldVerticalOffset = this.rowController.verticalOffset;
             this.mouseIsDown = true;
+            this.stateController.snapped = false;
+        };
+        const moveEnd = () => {
+            this.mouseIsDown = false;
+        }
+        this.addEvent('mouseover', (event) => {
+            if (this.stateController.snapped) {
+                moveStart(event);
+            }
         }, this._displayObject);
+        this.addEvent('mousedown', moveStart, this._displayObject);
         this.addEvent('mousemove', event => {
             if (this.mouseIsDown) {
                 const delta = (event.data.global.y - this.mouseStartY) / this.factor;
@@ -64,11 +80,9 @@ export class TimeGraphVerticalScrollbarHandle extends TimeGraphComponent<null> {
                 this.rowController.verticalOffset = Math.max(0, Math.min(this.rowController.totalHeight - this.stateController.canvasDisplayHeight, verticalOffset));
             }
         }, this._displayObject);
-        const moveEnd: TimeGraphInteractionHandler = event => {
-            this.mouseIsDown = false;
-        }
         this.addEvent('mouseup', moveEnd, this._displayObject);
         this.addEvent('mouseupoutside', moveEnd, this._displayObject);
+        document.addEventListener('snap-y-end', moveEnd);
     }
 
     updateFactor(factor: number) {
@@ -84,5 +98,52 @@ export class TimeGraphVerticalScrollbarHandle extends TimeGraphComponent<null> {
             width: 10,
             color: 0x777769
         })
+    }
+}
+
+export class TimeGraphVerticalScrollbarBackground extends TimeGraphComponent<null> {
+
+    protected snapEvent: CustomEvent;
+    protected snapEventString: string;
+
+    constructor(protected rowController: TimeGraphRowController, protected stateController: TimeGraphStateController, protected factor: number) {
+        super("vscroll_background");
+        this.addEvent("mousedown", event => {
+            let yPosition = event.data.getLocalPosition(this._displayObject).y;
+            let vOffset = (yPosition/this.stateController.canvasDisplayHeight) * this.rowController.totalHeight;
+            // We have vertical offset at point of click, but need to make it the center of scrollbar.
+            let scrollBarHeight = (this.rowController.totalHeight * this.factor);
+            vOffset = vOffset - (scrollBarHeight / 2);
+            // Clamp it
+            let vOffsetClamped = Math.max(0, Math.min(this.rowController.totalHeight - this.stateController.canvasDisplayHeight, vOffset));
+            this.rowController.verticalOffset = vOffsetClamped;
+            // Set snapped state
+            this.toggleSnappedState(true);
+        }, this._displayObject);
+        // Emit custom event to let vertical handler know when 'mousedown' is released.
+        this.snapEvent = new CustomEvent(this.snapEventString = 'snap-y-end');
+        const endSnap = () => {
+            this.toggleSnappedState(false);
+            document.dispatchEvent(this.snapEvent);
+        }
+        this.addEvent('mouseup', endSnap, this._displayObject);
+        this.addEvent('mouseupoutside', endSnap, this._displayObject);
+    }
+
+    updateFactor(factor: number) {
+        this.factor = factor;
+    }
+
+    protected toggleSnappedState = (bool: boolean) => {
+        this.stateController.snapped = bool;
+    }
+
+    render(): void {
+        this.rect({
+            height: this.stateController.canvasDisplayHeight,
+            position: { x: 0, y: 0 },
+            width: 10,
+            opacity: 0
+        });
     }
 }
